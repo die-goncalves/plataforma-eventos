c329dff74eb8c72af2c8a67ff7774e7752b4c6a2
die-goncalves <die.goncalves1990@gmail.com>
2022-06-23 22:02:21 -0300

    Adicionar fonte Roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif']
+    },
     extend: {
       colors: {
         green: {
