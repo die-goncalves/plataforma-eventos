52af6057ea06d44a0bbefd77d5bf1249e39b7658
die-goncalves <die.goncalves1990@gmail.com>
2022-06-23 21:43:33 -0300

    Adicionar configuração do tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+import './styles/global.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
