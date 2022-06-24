3849438f4cdb29d498b8fb315bc1f0846033e8e1
die-goncalves <die.goncalves1990@gmail.com>
2022-06-23 21:52:24 -0300

    Adicionar configuração do Apollo Client

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './lib/apollo'
 
 import './styles/global.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
